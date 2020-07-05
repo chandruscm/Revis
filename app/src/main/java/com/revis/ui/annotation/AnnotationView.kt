896d1834e9456c2ea1d7f55358de969b685ee5b4
Chandramohan Sudar <chandramohan.sudar@p3-group.com>
2020-07-05 14:37:56 +0200

    Add custom view for annotation

---
+++ app/src/main/java/com/revis/ui/annotation/AnnotationView.kt
@@ -0,0 +1,45 @@
+package com.revis.ui.annotation
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+import com.revis.R
+
+class AnnotationView : View {
+
+    private val pointerBitmap: Bitmap
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    constructor(
+        context: Context?,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(context, attrs, defStyleAttr, defStyleRes)
+
+    init {
+        pointerBitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_pointer_large)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+}
